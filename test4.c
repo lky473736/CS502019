3e9c0ccd2da27120590db3629487e3a4ce28bc24
임규연 <lky473736@icloud.com>
2023-05-17 13:44:36 +0900

    Update test4.c

--- test4.c
+++ test4.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <time.h> // 시간에 따라 rand 수열 리셋을 위해 time.h 필요
 
 void bubblesort(int arr[], int n) {
     int i, j;
-    for (i = 0; i < n-1; i++) {
-        for (j = 0; j < n-i-1; j++) {
+    for (i = 0; i < n-1; i++) { // cycle를 반복시키기 위해
+        for (j = 0; j < n-i-1; j++) { // 요소 단위로 하나씩 이동
             if (arr[j] > arr[j+1]) { // arr[j]가 arr[j+1]보다 크면 
                 // arr[j]와 arr[j+1]을 교환
                 int temp = arr[j];
